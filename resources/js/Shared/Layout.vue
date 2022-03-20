0838c4c766ab2b850a014ba610d9ff562bb6ddcb
Muhammad Luthfi Azzammi <luthfi_azzammi@treetan.com>
2022-03-20 14:37:50 +0700

    Shared Data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,9 +1,16 @@
 <template>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
-            <h1 class="font-bold text-lg">
-                My App
-            </h1>
+            <div class="flex items-center">
+                <h1 class="font-bold text-lg">
+                    My App
+                </h1>
+
+                <p class="text-sm ml-4">
+                    Welcome Back, {{ username }}
+                </p>
+            </div>
+
             <Nav />
         </header>
     </section>
@@ -20,5 +27,10 @@ import Nav from "./Nav";
         components: {
             Nav,
         },
+        computed: {
+            username() {
+                return this.$page.props.auth.user.username;
+            }
+        }
     };
 </script>
