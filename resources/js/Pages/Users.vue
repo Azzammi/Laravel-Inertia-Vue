c9aff48e71d8c9593a7e78f63be27764ede4b7b1
Muhammad Luthfi Azzammi <luthfi_azzammi@treetan.com>
2022-03-27 22:14:40 +0700

    An important SPA security

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -2,18 +2,16 @@
     <Head>
         <title>Users</title>
     </Head>
-    <h1>Users</h1>
-    <div style="margin-top: 400px">
-        <p>The current time is {{time}}</p>
-    </div>
+    <h1 text="text-3xl">Users</h1>
 
-    <Link
-        href="/users"
-        class="text-blue-500"
-        preserve-scroll>
-        Refresh
-    </Link>
+    <ul>
+        <li
+        v-for="user in users"
+        :key="user.id"
+        v-text="user.name"
+        />
+    </ul>
 </template>
 <script setup>
-    defineProps({time:String});
+    defineProps({ users: Array});
 </script>
